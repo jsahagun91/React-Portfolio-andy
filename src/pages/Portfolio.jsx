d9c8fbd1a5441b974e5988de723ebd2e7538d8b8
Andy Godoy <Andygodoy@ymail.com>
2023-09-21 20:29:31 -0700

    trying again

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -12,12 +12,12 @@ export default function Portfolio(){
     }
     
     const projects = [
-        new Project('DTC SOLUTIONS', 'Public/check-engine-light-icon-1616189100.jpg', 'https://github.com/jacob-kachmarek/dtc-solutions', 'https://jacob-kachmarek.github.io/dtc-solutions/'),
-        new Project("SillyCode Valley!", 'Public/1654472780-improv_header_final.jpg', 'https://github.com/Chapjae/sillyCode_valley', 'https://intense-sands-40473-ea199ebca0d8.herokuapp.com/'),
-        new Project("Weather App", "Public/Weather_iOS_15.webp", "https://github.com/AndyGodoy/WeatherApp_Project-", "https://andygodoy.github.io/WeatherApp_Project-/"),
-        new Project("ReadMe Generator", "Public/read-me-640-20140826113452850.jpg", "https://github.com/AndyGodoy/ReadMe-Generator", "https://github.com/AndyGodoy/ReadMe-Generator/blob/main/README.md"),
-        new Project("Fun Work Scheduler", "Public/work-schedules-header-banner.png", "https://github.com/AndyGodoy/Fun_Work_Schedule", "https://andygodoy.github.io/Fun_Work_Schedule/"),
-        new Project("E-Commerce BackEnd", "Public/ecommerce3-1.jpg", "https://github.com/AndyGodoy/E-commerce_BackEnd", "https://drive.google.com/file/d/1Y2HKJPu_d4VrItNzLosJyaWUdvzlkXfl/view?pli=1"),
+        new Project('DTC SOLUTIONS', '/check-engine-light-icon-1616189100.jpg', 'https://github.com/jacob-kachmarek/dtc-solutions', 'https://jacob-kachmarek.github.io/dtc-solutions/'),
+        new Project("SillyCode Valley!", '/1654472780-improv_header_final.jpg', 'https://github.com/Chapjae/sillyCode_valley', 'https://intense-sands-40473-ea199ebca0d8.herokuapp.com/'),
+        new Project("Weather App", "/Weather_iOS_15.webp", "https://github.com/AndyGodoy/WeatherApp_Project-", "https://andygodoy.github.io/WeatherApp_Project-/"),
+        new Project("ReadMe Generator", "/read-me-640-20140826113452850.jpg", "https://github.com/AndyGodoy/ReadMe-Generator", "https://github.com/AndyGodoy/ReadMe-Generator/blob/main/README.md"),
+        new Project("Fun Work Scheduler", "/work-schedules-header-banner.png", "https://github.com/AndyGodoy/Fun_Work_Schedule", "https://andygodoy.github.io/Fun_Work_Schedule/"),
+        new Project("E-Commerce BackEnd", "/ecommerce3-1.jpg", "https://github.com/AndyGodoy/E-commerce_BackEnd", "https://drive.google.com/file/d/1Y2HKJPu_d4VrItNzLosJyaWUdvzlkXfl/view?pli=1"),
     ]
 
 
