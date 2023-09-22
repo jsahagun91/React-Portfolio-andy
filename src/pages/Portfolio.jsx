9d71dcfa073b1d5d244db81107df10a9c9884960
Andy Godoy <Andygodoy@ymail.com>
2023-09-21 22:26:26 -0700

    added slash

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -11,12 +11,12 @@ export default function Portfolio(){
     }
     
     const projects = [
-        new Project('DTC SOLUTIONS', 'engineLight.jpg', 'https://github.com/jacob-kachmarek/dtc-solutions', 'https://jacob-kachmarek.github.io/dtc-solutions/'),
-        new Project("SillyCode Valley!", 'improv.jpg', 'https://github.com/Chapjae/sillyCode_valley', 'https://intense-sands-40473-ea199ebca0d8.herokuapp.com/'),
-        new Project("Weather App", "weatherIos.webp", "https://github.com/AndyGodoy/WeatherApp_Project-", "https://andygodoy.github.io/WeatherApp_Project-/"),
-        new Project("ReadMe Generator", "readMe.jpg", "https://github.com/AndyGodoy/ReadMe-Generator", "https://github.com/AndyGodoy/ReadMe-Generator/blob/main/README.md"),
-        new Project("Fun Work Scheduler", "workSchedule.png", "https://github.com/AndyGodoy/Fun_Work_Schedule", "https://andygodoy.github.io/Fun_Work_Schedule/"),
-        new Project("E-Commerce BackEnd", "ecommerce.jpg", "https://github.com/AndyGodoy/E-commerce_BackEnd", "https://drive.google.com/file/d/1Y2HKJPu_d4VrItNzLosJyaWUdvzlkXfl/view?pli=1"),
+        new Project('DTC SOLUTIONS', '/engineLight.jpg', 'https://github.com/jacob-kachmarek/dtc-solutions', 'https://jacob-kachmarek.github.io/dtc-solutions/'),
+        new Project("SillyCode Valley!", '/improv.jpg', 'https://github.com/Chapjae/sillyCode_valley', 'https://intense-sands-40473-ea199ebca0d8.herokuapp.com/'),
+        new Project("Weather App", "/weatherIos.webp", "https://github.com/AndyGodoy/WeatherApp_Project-", "https://andygodoy.github.io/WeatherApp_Project-/"),
+        new Project("ReadMe Generator", "/readMe.jpg", "https://github.com/AndyGodoy/ReadMe-Generator", "https://github.com/AndyGodoy/ReadMe-Generator/blob/main/README.md"),
+        new Project("Fun Work Scheduler", "/workSchedule.png", "https://github.com/AndyGodoy/Fun_Work_Schedule", "https://andygodoy.github.io/Fun_Work_Schedule/"),
+        new Project("E-Commerce BackEnd", "/ecommerce.jpg", "https://github.com/AndyGodoy/E-commerce_BackEnd", "https://drive.google.com/file/d/1Y2HKJPu_d4VrItNzLosJyaWUdvzlkXfl/view?pli=1"),
     ]
 
 
