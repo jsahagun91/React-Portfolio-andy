dc806903d8aaad4d0e78cd08fc04363626f59e23
Andy Godoy <Andygodoy@ymail.com>
2023-09-23 12:48:50 -0700

    trying the about me

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,15 @@
-
+import me from "/me.jpg";
 
 export default function About() {
   return (
     <div className="d-flex my-3">
-    <img src="/me.jpg" className="my-face mx-3" alt="my-face" />
+    <img id="/me.jpg" src={me} className="my-face mx-3" alt="my-face" />
       <p className='mx-5 text-center d-flex align-items-center'>
         Versatile and results-driven professional with a passion for learning and an unwavering dedication to excellence. Automotive industry expert with a background in technical troubleshooting and certified in Auto, Diesel, and Toyota servicing.
 
         Throughout my rewarding tenure at both Tesla and Rivian, I had the incredible opportunity to collaborate closely with talented engineers, witnessing firsthand the impact of cutting-edge technologies on the automotive landscape. This inspiring experience ignited a newfound passion for learning to code and understanding the power of software development.
 
-        Currently, I'm undergoing a transformative 3-month coding bootcamp, eagerly pivoting into a dynamic career in software development. My aim is to merge my technical acumen from the automotive industry with innovative problem-solving skills, offering valuable contributions to a forward-thinking tech team.
+        Currently, I am undergoing a transformative 3-month coding bootcamp, eagerly pivoting into a dynamic career in software development. My aim is to merge my technical acumen from the automotive industry with innovative problem-solving skills, offering valuable contributions to a forward-thinking tech team.
       </p>
     </div>
   )
